03b8e4554a3cf339f0e8660fed6f3b918f4ebf7e
DESKTOP-7JANDEU\뚜니 <soointhehouse@naver.com>
2025-09-03 17:34:28 +0900

    team-project 상세페이지 (반응형 수정)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import ToastDemo from "./pages/ToastDemo.jsx";
 import CreateStudyPage from "./pages/CreateStudyPage";
-import './styles/fonts.scss';
 import "./styles/global.scss";
 
 export default function App() {
