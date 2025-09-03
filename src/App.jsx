4b66eadea6fc26b3cd08e79a12aae86818a67072
MinJun916 <ed125248@gmail.com>
2025-09-03 22:54:59 +0900

    refactor: update App.jsx, main.jsx, and vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import ToastDemo from "./pages/ToastDemo.jsx";
 import CreateStudyPage from "./pages/CreateStudyPage";
+import Workshop from "./pages/Workshop.jsx";
 import "./styles/global.scss";
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />   {/* / 경로 */}
-        <Route path="toast" element={<ToastDemo />} />
-        <Route path="/study/new" element={<CreateStudyPage />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} /> {/* / 경로 */}
+          <Route path="/workshop" element={<Workshop />} />
+          <Route path="toast" element={<ToastDemo />} />
+          <Route path="/study/new" element={<CreateStudyPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,17 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "@pages/Home.jsx";
-import Workshop from "@pages/Workshop.jsx";
+import App from "./App.jsx";
 import "./styles/globals.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/workshop" element={<Workshop />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
