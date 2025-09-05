f57e0380c088869a25aab944865fdf56ac9a0677
kwangche <163306559+kwangche@users.noreply.github.com>
2025-09-05 13:54:25 +0900

    feat: Tag 컴포넌트 추가 및 스타일 정의

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Focus from '@pages/Focus.jsx';
 import ToastDemo from '@pages/ToastDemo.jsx';
 import Emoji from './components/Emoji/Emoji';
 import DropDown from './components/DropDown/DropDown';
+import Tag from './components/Tag/Tag';
 /* 위는 테스트 페이지입니다 나중에 삭제요망 */
 
 import '@/styles/global.scss';
@@ -33,6 +34,7 @@ export default function App() {
             <Route path="toast" element={<ToastDemo />} />
             <Route path="emoji" element={<Emoji studyId='6b78c2cd-cc98-44ea-810e-2c68d46ab6a4'/>} />
             <Route path="dropdown" element={<DropDown />} />
+            <Route path="tag" element={<Tag bgColor={'rgba(0,0,0,0.5)'} fontSize={12} studyId={'6b78c2cd-cc98-44ea-810e-2c68d46ab6a4'}/>} />
           </Route>
           {/* 위는 테스트 페이지입니다 나중에 삭제요망 */}
         </Route>
