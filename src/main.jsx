3aae77ef39fd5fe6f890d538b832015777a2890f
MinJun916 <ed125248@gmail.com>
2025-09-07 21:29:33 +0900

    fix: apply font in Header and remove Test.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,4 @@ import { createRoot } from 'react-dom/client';
 import App from '@/App.jsx';
 import '@/styles/global.scss';
 
-// TEST CODE
-import Test from '@/Test.jsx';
-
 createRoot(document.getElementById('root')).render(<App />);
