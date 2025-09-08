a74cce740a4d2c95eaa6041ce3a6fbdc48ef5ba5
MinJun916 <ed125248@gmail.com>
2025-09-08 18:25:09 +0900

    refactor: add onValueChange prop to TextArea component for parent usage

--- src/components/input/TextArea.jsx
+++ src/components/input/TextArea.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import styles from '@styles/components/input/TextArea.module.scss';
 
-function TextArea() {
+function TextArea({ onValueChange }) {
+  const [textArea, setTextArea] = useState('');
+
+  const handleTextAreaChange = (e) => {
+    setTextArea(e.target.value);
+    onValueChange(textArea);
+    console.log(textArea);
+  };
+
   return (
     <div>
-      <textarea className={styles.textarea} placeholder="소개 멘트를 작성해 주세요" />
+      <textarea
+        className={styles.textarea}
+        placeholder="소개 멘트를 작성해 주세요"
+        value={textArea}
+        onChange={handleTextAreaChange}
+      />
     </div>
   );
 }
