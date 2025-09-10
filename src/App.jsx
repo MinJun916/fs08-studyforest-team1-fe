ffa4979748d5d65c83d265e57682bbeb316aebcb
MinJun916 <ed125248@gmail.com>
2025-09-11 04:05:19 +0900

    refactor: remove unused files and reorder App.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@pages/Home.jsx';
 import CreateStudyPage from '@pages/CreateStudyPage.jsx';
 import DetailStudyPage from '@pages/DetailStudyPage.jsx';
+import Habit from '@/pages/Habit';
 import Focus from '@pages/Focus.jsx';
 import NotFoundPage from '@pages/NotFoundPage.jsx';
-import EmojiList from '@components/emoji/EmojiList';
-import Habit from '@/pages/Habit';
-import Timer from '@/components/timer/Timer';
 
 export default function App() {
   return (
